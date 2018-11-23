907a6afe8fd5e30cb9bf459f6174ba52d9456a81
Filipe Borba <filipe.borba@grad.ufsc.br>
2018-11-23 21:42:32 -0200

    Add source file test

--- runner.py
+++ runner.py
@@ -31,3 +31,11 @@ except:
 
 print('Chegou no fim do arquivo?')
 print(lex.next_token().type == lexer.TokenType.EOF)
+
+print('Testando source.c')
+lex = lexer.Lexer('data/source.c')
+t = lex.next_token()
+while t.type != lexer.TokenType.EOF:
+    print(t.type.name)
+    t = lex.next_token()
+print('Leitura bem sucedida!')
