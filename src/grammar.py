03761d9f53de1132b8bf3b5812de06b0c241b24c
lucas <lucasjoao.lj@gmail.com>
2018-11-25 17:00:44 -0200

    Refactory grammar.py
    - Removed unnecessary code;
    - Fixed pylint warnings;
    - Added necessary FIXMEs to work with CFG

--- src/grammar.py
+++ src/grammar.py
@@ -1,11 +1,10 @@
 import json
 
-from .utils import Utils
 
-class Grammar(object):
+class Grammar:
 
     def __init__(self, productions, initial_symbol):
-        """Constructs a regular grammar from a list of productions and the initial symbol.
+        """Constructs a grammar from a list of productions and the initial symbol.
 
         Assumes each production "A -> aB", where B is optional, is of the form:
             ("A", "a"[, "B"])
@@ -14,15 +13,6 @@ class Grammar(object):
         self._productions = set(productions)
         self._nonterminals = self._get_nonterminals()
         self._terminals = self._get_terminals()
-
-    def to_automaton(self):
-        from .automata import Automata
-        transitions = self._make_transitions()
-        final_states = set(Utils.NEW_FINAL_STATE)
-        q0 = self._initial_symbol
-        states = self._nonterminals | final_states
-        alphabet = self._terminals
-        return Automata(alphabet, states, q0, final_states, transitions)
 
     def save_json(self, filename):
         data = {
@@ -38,6 +28,7 @@ class Grammar(object):
         nonterminals = set(self._initial_symbol)
         for production in self._productions:
             nonterminals.add(production[0])
+            # FIXME: maybe use save_json to get nonterminals
             if len(production) == 3:
                 nonterminals.add(production[2])
         return nonterminals
@@ -45,28 +36,12 @@ class Grammar(object):
     def _get_terminals(self):
         terminals = set()
         for production in self._productions:
-            if production[1] != Utils.EPSILON:
-                terminals.add(production[1])
+            # FIXME: maybe use save_json to get terminals
+            # if production[1] != Utils.EPSILON:
+            terminals.add(production[1])
         return terminals
 
-    def _make_transitions(self):
-        transitions = dict()
-        for production in self._productions:
-            input = Utils.TRANSITION(production[0], production[1])
-            output = self._get_next_state(production)
-            self._include_transition(transitions, input, output)
-        return transitions
-
-    def _get_next_state(self, production):
-        if len(production) == 3:
-            return production[2]
-        return Utils.NEW_FINAL_STATE
-
-    def _include_transition(self, transitions, input, output):
-        if not input in transitions.keys():
-            transitions[input] = list()
-        transitions[input].append(output)
-
+    @staticmethod
     def read_from_json(filename):
         with open(filename + '.json', 'r') as read_file:
             data = json.load(read_file)
